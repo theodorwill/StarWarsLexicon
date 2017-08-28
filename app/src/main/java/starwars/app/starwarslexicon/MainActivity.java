d23c2e1a3a317e1896f4b4d85b6106b21b103760
cba <theodor.will@gmail.com>
2017-08-28 14:18:56 +0200

    Initial commit

---
+++ app/src/main/java/starwars/app/starwarslexicon/MainActivity.java
@@ -0,0 +1,96 @@
+package starwars.app.starwarslexicon;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import starwars.app.starwarslexicon.fragments.PeopleFragment;
+import starwars.app.starwarslexicon.fragments.PlanetsFragment;
+import starwars.app.starwarslexicon.fragments.SearchFragment;
+import starwars.app.starwarslexicon.fragments.ShipsFragment;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.contentFrame, new SearchFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        FragmentManager fm = getFragmentManager();
+        int id = item.getItemId();
+
+        if (id == R.id.nav_search) {
+            fm.beginTransaction().replace(R.id.contentFrame, new SearchFragment()).commit();
+        } else if (id == R.id.nav_people) {
+            fm.beginTransaction().replace(R.id.contentFrame, new PeopleFragment()).commit();
+        } else if (id == R.id.nav_planets) {
+            fm.beginTransaction().replace(R.id.contentFrame, new PlanetsFragment()).commit();
+        } else if (id == R.id.nav_ships) {
+            fm.beginTransaction().replace(R.id.contentFrame, new ShipsFragment()).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/starwars/app/starwarslexicon/fragments/PlanetsFragment.java
@@ -0,0 +1,26 @@
+package starwars.app.starwarslexicon.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import starwars.app.starwarslexicon.R;
+
+/**
+ * Created by cba on 2017-08-28.
+ */
+
+public class PlanetsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_planets, container, false);
+
+        return rootView;
+    }
+}
